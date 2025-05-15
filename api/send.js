3a9926c284a712132ffd2a28a593a2ca2bec3341
Your Full NameJohnny <youremail@example.comyohealthmowealth@gmail.com>
2025-05-15 16:12:40 -0700

    Updates

--- api/send.js
+++ api/send.js
@@ -1,10 +1,21 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://script.google.com/macros/s/AKfycbwiNgOcIQqiZApI33mQmkxuiv45kkadkr5wTWtBRtP_51ZYXbO7E0P0TjUuk4XZH8Eh/exec", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: new URLSearchParams(req.body)
+  if (req.method !== 'POST') {
+    res.status(405).send('Method Not Allowed');
+    return;
+  }
+
+  const { name, email, results } = req.body;
+
+  const formData = new URLSearchParams({
+    name,
+    email,
+    results
+  });
+
+  const response = await fetch('https://script.google.com/macros/s/AKfycbwiNgOcIQqiZApI33mQmkxuiv45kkadkr5wTWtBRtP_51ZYXbO7E0P0TjUuk4XZH8Eh/exec', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: formData
   });
 
   const text = await response.text();
